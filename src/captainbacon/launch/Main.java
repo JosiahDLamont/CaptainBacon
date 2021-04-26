92dfff591b6d419dd65be250177b75f6a2d43cf6
Josiah Lamont <Jshgamer@msn.com>
2021-04-26 18:51:49 -0400

    Renamed Classes in launch package for clarity.

--- src/captainbacon/launch/Main.java
+++ src/captainbacon/launch/Main.java
@@ -7,7 +7,7 @@ import captainbacon.util.logging.Logger;
  * This class contains the main method and launches the controller and the view
  * according to the provided command line arguments.
  */
-public class Launcher {
+public class Main {
     public static final Logger LOGGER = new Logger();
 
     public static void main(String[] args) {
@@ -25,6 +25,6 @@ public class Launcher {
         LOGGER.start();
 
         // Creates the Captain Bacon client and starts the GUI.
-        ClientLauncher.launch(ClientLauncher.class, args);
+        FXLauncher.launch(FXLauncher.class, args);
     }
 }
